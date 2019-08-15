9a6645a5250bc2b4a57201b84c9e7ff9b05d536b
marzmc <marboy1000@gmail.com>
2019-08-15 16:10:12 +0200

    Add files via upload

--- HgKleineEvents.java
+++ HgKleineEvents.java
@@ -1,10 +1,14 @@
 package me.Miracles.Hg;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
+
+import com.avaje.ebeaninternal.server.cluster.mcast.IncomingPacketsProcessed.GotAllPoint;
 
 public class HgKleineEvents implements Listener{
 	public void block(BlockBreakEvent even) {
@@ -23,6 +27,14 @@ public class HgKleineEvents implements Listener{
 	public void weat1(FoodLevelChangeEvent even) {
 		if(HgMain.GameProgress == "Wachtend") {
 			even.setCancelled(true);
+		}
+	}
+	public void weat2(PlayerMoveEvent even) {
+		if(HgMain.GameProgress == "Starting") {
+			Player pla = even.getPlayer();
+			if(even.getFrom() != even.getTo()) {
+				even.setCancelled(true);
+			}
 		}
 	}
 	
